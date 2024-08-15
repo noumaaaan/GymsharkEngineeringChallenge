77c9b15868ce388dbc2f8a5ba66a6bd3c7ecd6a5
Nouman Mehmood <nouman_7@live.co.uk>
2024-08-15 01:27:26 +0100

    Initial Commit

---
+++ GymsharkEngineeringChallenge/GymsharkEngineeringChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GymsharkEngineeringChallengeApp.swift
+//  GymsharkEngineeringChallenge
+//
+//  Created by Noumaan Mehmood on 15/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct GymsharkEngineeringChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
